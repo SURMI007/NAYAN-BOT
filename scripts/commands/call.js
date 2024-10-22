95de55063a59d88e5cf1a12e607709a9e56962c9
N4Y4N X COPY <maxjihad59@gmail.com>
2024-10-22 07:20:01 +0600

    Create call.js

---
+++ scripts/commands/call.js
@@ -0,0 +1,51 @@
+module.exports = {
+  config: {
+  name: "call",
+  version: "1.0.0",
+  permission: 2,
+  credits: "LIKHON6T9X",
+  prefix: 'awto',
+  description: "CALL Bombing",
+  category: "without prefix",
+  cooldowns: 5
+},
+
+start: async function({ nayan, args, events, Users, NAYAN }) {
+  try {
+
+  const axios = require("axios")
+  const info = args.join(" ")
+  const msg = info.split("-");
+  const number = msg[0].trim();
+  const limit = msg[1].trim();
+
+  if (!info){
+    NAYAN.react("❌")
+    return nayan.reply(" Number & limit not found", events.threadID, events.messageID)
+  }
+
+  if (!number || !limit){
+    NAYAN.react("❌")
+    return nayan.reply("Number & limit not found", events.threadID, events.messageID)
+  }
+
+    NAYAN.react("⏳")
+
+    const res = await axios.get(`http://5.9.12.94:15280/call?mobileNo=${number}&countryDialingCode=880`)
+
+  const data = res.data.status
+    if (data.error){
+      NAYAN.react("🖕")
+      return nayan.reply(data.error, events.threadID, events.messageID)
+
+    }
+
+
+
+  nayan.reply(data, events.threadID, events.messageID)
+
+} catch (error){
+    nayan.reply("Sms not send", events.threadID, events.messageID)
+}
+}
+    }
